0c31da064d5d8ef56edff555dbffe75cab65f480
Krzysztof Spytkowski <kspytkowski@wp.pl>
2014-09-22 20:49:59 +0200

    1 and 2 exercise done

--- week1/recfun/src/main/scala/recfun/Main.scala
+++ week1/recfun/src/main/scala/recfun/Main.scala
@@ -14,15 +14,26 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = if (c == 0 || c == r) 1 else pascal(c - 1, r - 1) + pascal(c, r - 1)
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def loop(numberOfLeftParenthesis: Int, numberOfRightParenthesis: Int, chars: List[Char]): Boolean = {
+      if (numberOfRightParenthesis > numberOfLeftParenthesis) false
+      else if (chars.isEmpty) (numberOfLeftParenthesis == numberOfRightParenthesis)
+      else if (chars.head == '(') loop(numberOfLeftParenthesis + 1, numberOfRightParenthesis, chars.tail)
+      else if (chars.head == ')') loop(numberOfLeftParenthesis, numberOfRightParenthesis + 1, chars.tail)
+      else loop(numberOfLeftParenthesis, numberOfRightParenthesis, chars.tail)
+    }
+    loop(0, 0, chars)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+  }
+  
 }
