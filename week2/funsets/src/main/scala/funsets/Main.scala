7ba798783abad64a82e35d9a1b6b6c20d7902357
Krzysztof Spytkowski <kspytkowski@wp.pl>
2014-09-23 00:52:37 +0200

    working with new definition of set - map still to implement

--- week2/funsets/src/main/scala/funsets/Main.scala
+++ week2/funsets/src/main/scala/funsets/Main.scala
@@ -3,4 +3,6 @@ package funsets
 object Main extends App {
   import FunSets._
   println(contains(singletonSet(1), 1))
+  
+  println(FunSets.toString(map(x => x == 1 || x == 3 || x == 4 || x == 5 || x == 7 || x == 1000, x => x - 1)))
 }
